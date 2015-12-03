6d33f83b3b8c5c97c20a2ff06d37074c87b1a5b9
goahead1987 <kuaisheng@126.com>
2015-12-03 17:03:34 +0800

    xx

--- models/Models.js
+++ models/Models.js
@@ -5,11 +5,12 @@ var Seq = require('sequelize');
 var connectDB = require('main-dir/helpers/connectDB');
 var Models = {};
 
-/**用户信息***/
-Models.User = connectDB.define('user', {
+/***用户信息***/
+var User = Models.User = connectDB.define('user', {
     id: {
         type: Seq.INTEGER,
         primaryKey: true, // 定义主键
+        unique: true,
         autoIncrement: true //自动递增
     },
     account: {
@@ -43,8 +44,87 @@ Models.User = connectDB.define('user', {
         type: Seq.STRING(64),
         comment: "邮箱"
     }
-}, {
-    underscored: true
+});
+
+/***用户和朋友关联关系***/
+var UserMid = Models.UserMid  = connectDB.define('user_mid', {
+    id: {
+        type: Seq.INTEGER,
+        primaryKey: true, // 定义主键
+        unique: true,
+        autoIncrement: true //自动递增
+    },
+    relate: {
+        type: Seq.INTEGER(2),
+        defaultValue: 0,
+        comment: "熟识程度"
+    },
+    relateType: {
+        type: Seq.INTEGER(2),
+        field: 'relate_type',
+        defaultValue: 0,
+        comment: "关系类型，不认识0，直接认识1，间接认识2，"
+    },
+    contactWay: {
+        type: Seq.INTEGER(2),
+        allowNull: false,
+        field: 'contact_way',
+        comment: "相识途径，账号查找0，朋友帮助1"
+    },
+    midUserId: {
+        type: Seq.INTEGER,
+        field: 'mid_user_id',
+        comment: "相识中间人id"
+    },
+    name: {
+        type: Seq.STRING(64),
+        comment: "朋友的备注姓名"
+    }
+});
+User.belongsToMany(User, {as: 'Root', through: UserMid});
+
+/***消息***/
+var Msg = Models.Msg  = connectDB.define('msg', {
+    id: {
+        type: Seq.INTEGER,
+        primaryKey: true,
+        unique: true,
+        autoIncrement: true
+    },
+    content: {
+        type: Seq.STRING,
+        comment: "文字内容"
+    },
+    imgUrls: {
+        type: Seq.STRING,
+        field: 'img_urls',
+        comment: "图片地址串，“:”号分割"
+    }
+});
+User.hasMany(Msg);
+
+/***评论***/
+var Comment = Models.Comment  = connectDB.define('comment', {
+    id: {
+        type: Seq.INTEGER,
+        primaryKey: true,
+        unique: true,
+        autoIncrement: true
+    },
+    content: {
+        type: Seq.STRING,
+        comment: "文字内容"
+    },
+    fromUser: {
+        type: Seq.INTEGER,
+        allowNull: false,
+        field: 'from_user'
+    },
+    toUser: {
+        type: Seq.INTEGER,
+        allowNull: false,
+        field: 'to_user'
+    }
 });
 
 module.exports = Models;
--- tasks/creatDbAndModel.js
+++ tasks/creatDbAndModel.js
@@ -6,13 +6,26 @@ var gutil = require("gulp-util");
 var Models = require('main-dir/models/Models');
 
 var User = Models.User;
+var UserMid = Models.UserMid;
+var Msg = Models.Msg;
 
 module.exports = {
-    task: function(gulp) {
-        User.sync({force: true})
-        //User.sync()
-        .then(function (res) {
+    task: function (gulp) {
+        User.sync()
+            .then(function (res) {
                 gutil.log('创建User成功', res);
+            })
+            .then(function () {
+                return UserMid.sync()
+            })
+            .then(function (res) {
+                gutil.log('创建UserMid成功', res);
+            })
+            .then(function () {
+                return Msg.sync()
+            })
+            .then(function (res) {
+                gutil.log('创建Msg成功', res);
             });
     }
 };
